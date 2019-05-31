c4b4ddb6ad99d0c70f10c8781a19b10bf49d60f1
josh <joshgrime@responstapsmbp3.home>
2019-05-31 13:25:13 +0100

    .

--- src/pages/Set-Time.js
+++ src/pages/Set-Time.js
@@ -4,12 +4,16 @@ class SetTime extends React.Component {
     constructor(){
         super();
         this.state = {
+            loaded: false,
             thisMonthCalendar: null,
             nextMonthCalendar: null,
             thisMonth:null,
             nextMonth:null,
             daySelected:null,
-            timeSelected:null
+            timesSelected:[],
+            timesSelectedMap:[],
+            timesShown:[],
+            timesShownMap:[]
         }
         this.days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
         this.months = [
@@ -62,100 +66,7 @@ class SetTime extends React.Component {
                 days: 31
             }
         ];
-    }
-
-    
-
-    componentDidMount(){
-        var t = this;
-        var date = new Date();
-        var currentMonth = date.getMonth();
-        var december = false;
-        var nextMonth = currentMonth + 1;
-        var currentYear = date.getFullYear();
-        var nextYear = currentYear;
-        if (currentMonth === 11) {
-            december = true;
-        }
-        if (december) {
-            nextMonth = 0;
-            nextYear = currentYear + 1;
-        } //add leap year logic
-        var currentMonthStartDay = new Date('01-'+this.months[currentMonth].name+'-'+currentYear);
-        var nextMonthStartDay = new Date('01-'+this.months[nextMonth].name+'-'+nextYear);
-
-        var day1cm = currentMonthStartDay.toString().substring(0,3);
-        var day1nm = nextMonthStartDay.toString().substring(0,3);
-
-        var startDayIndex = this.days.indexOf(day1cm);
-        var startDayIndex2 = this.days.indexOf(day1nm);
-
-        var thisMonthCalendarIndex = makeCal(this.months[currentMonth].days, startDayIndex);
-        var nextMonthCalendarIndex = makeCal(this.months[nextMonth].days, startDayIndex2);
-
-        var tmc = this.days.concat(thisMonthCalendarIndex);
-        var nmc = this.days.concat(nextMonthCalendarIndex);
-
-        var thisMonthCalendar = tmc.map(x => {
-           return mapCal(x); 
-        });
-
-        var nextMonthCalendar = nmc.map(x => {
-            return mapCal(x);
-        });
-
-        this.setState({thisMonthCalendar: thisMonthCalendar, nextMonthCalendar: nextMonthCalendar, thisMonth: this.months[currentMonth].name, nextMonth: this.months[nextMonth].name});
-
-        function handleClick(e){
-            var s = document.getElementsByClassName('calendarSquare-selected');
-            for (let x of s) {
-                x.className = 'calendarSquare';
-            }
-            e.target.className = 'calendarSquare-selected';
-            t.setState({daySelected:e.target.innerHTML});
-        }
-        
-        function makeCal(days, sdi){
-            var pushCounter = 0;
-            var arr = [];
-            for (let i=1; i<43; i++) {
-                if (i>days + pushCounter) {
-                    break;
-                }
-                if (i-1<sdi) {
-                    pushCounter++;
-                    arr.push(null);
-                }
-                else {
-                    var day = i - pushCounter;
-                    arr.push(day)
-                }
-            }
-            return arr;
-        }
-
-        function mapCal(x){
-            var content;
-            var oc;
-            var className = 'calendarSquare'
-            if (typeof x == 'string') {
-                className = 'calendarSquare-day'
-            }
-            if (x !== null) {
-                content = x;
-                if (typeof x == 'number') {
-                    oc = handleClick;
-                }
-            }
-            else {
-                className = 'calendarSquareg';
-            }
-            return <div className={className} onClick={oc}>{content}</div>;
-        }
-    }
-
-    timeSliderChange = (e) => {
-        var map = [
+        this.hours = [
             '00:00',
             '00:30',
             '01:30',
@@ -204,18 +115,149 @@ class SetTime extends React.Component {
             '23:00',
             '23:30'
         ];
-        console.log(e.value);
-        this.setState({timeSelected:map[map.length-e.target.value-1]});
+    }
+
+
+    createCalendars = () => {
+        var t = this;
+        var date = new Date();
+        var currentMonth = date.getMonth();
+        var december = false;
+        var nextMonth = currentMonth + 1;
+        var currentYear = date.getFullYear();
+        var nextYear = currentYear;
+        if (currentMonth === 11) {
+            december = true;
+        }
+        if (december) {
+            nextMonth = 0;
+            nextYear = currentYear + 1;
+        } //add leap year logic
+        var currentMonthStartDay = new Date('01-'+this.months[currentMonth].name+'-'+currentYear);
+        var nextMonthStartDay = new Date('01-'+this.months[nextMonth].name+'-'+nextYear);
+
+        var day1cm = currentMonthStartDay.toString().substring(0,3);
+        var day1nm = nextMonthStartDay.toString().substring(0,3);
+
+        var startDayIndex = this.days.indexOf(day1cm);
+        var startDayIndex2 = this.days.indexOf(day1nm);
+
+        var thisMonthCalendarIndex = makeCal(this.months[currentMonth].days, startDayIndex, currentMonth, currentYear);
+        var nextMonthCalendarIndex = makeCal(this.months[nextMonth].days, startDayIndex2, nextMonth, nextYear);
+
+        var tmc = this.days.concat(thisMonthCalendarIndex);
+        var nmc = this.days.concat(nextMonthCalendarIndex);
+
+        var thisMonthCalendar = tmc.map(x => {
+           return mapCal(x); 
+        });
+
+        var nextMonthCalendar = nmc.map(x => {
+            return mapCal(x);
+        });
+
+        function handleClick(e){
+            var str = `${e.x.day}/${e.x.month}/${e.x.year}`;
+            //get already selected times for this day from server and set timesShown and timesSelected states
+            console.log(t.hours);
+            t.setState({daySelected:{obj:e.x,str:str}, timesShown:t.hours});
+        }
+
+        function makeCal(days, sdi, m, y){
+            var pushCounter = 0;
+            var arr = [];
+            for (let i=1; i<43; i++) {
+                if (i>days + pushCounter) {
+                    break;
+                }
+                if (i-1<sdi) {
+                    pushCounter++;
+                    arr.push(null);
+                }
+                else {
+                    var day = i - pushCounter;
+                    arr.push({day:day, month: m, year: y});
+                }
+            }
+            return arr;
+        }
+
+        function mapCal(x){
+            console.log(x);
+            var content;
+            var oc;
+            var className = 'calendarSquare';
+            if (typeof x == 'string') {
+                content = x;
+                className = 'calendarSquare-day'
+            }
+            if (x !== null) {
+                if (x.day !== undefined) {
+                    oc = function() {handleClick({x})};
+                    content = x.day;
+                }
+            }
+            else {
+                className = 'calendarSquareg';
+            }
+            return <div className={className} onClick={oc}>{content}</div>;
+        }
+
+        this.setState({thisMonthCalendar: thisMonthCalendar, nextMonthCalendar: nextMonthCalendar, thisMonth: this.months[currentMonth].name, nextMonth: this.months[nextMonth].name});
+    }
+
+    handleTimeClick = (x,f) => {
+        if (f === 'time') {
+            var a = this.state.timesSelected;
+            var b = this.state.timesShown;
+            a.push(x);
+            var i = b.indexOf(x);
+            b.splice(i,1);
+            this.setState({timesSelected:a, timesShown:b});
+        }
+    }
+    
+    handleReset = (x) => {
+        console.log('reset with ');
+        console.log(x);
+        this.setState({timesSelected:[], timesShown: x});
+    }
+
+    componentDidMount(){
+        if (!this.state.loaded) {
+            this.createCalendars();
+            this.setState({loaded:true});
+        }
     }
 
     render () {
 
+        var allTimes = this.state.timesShown.map(x=>{
+            return <div className="time" onClick={()=>{this.handleTimeClick(x,'time')}}>{x}</div>;
+        });
+
+        var timesSelectedMap = this.state.timesSelected.map(x=>{
+        return <div className="time-selected" onClick={()=>{this.handleTimeClick(x,'time-selected')}}>{x}</div>;
+        });
+
+
+        var resetBtn;
+        if (timesSelectedMap.length > 0) {
+            resetBtn = <button onClick={()=>{this.handleReset(this.hours)}}>Reset Day's Selections</button>
+        }
+
         var timeSelector;
         if (this.state.daySelected !== null) {
-            timeSelector = <div>Select time for {this.state.daySelected}<p />
-              {this.state.timeSelected}
-                <input type="range" className="setTimeInput" orient="vertical" min="0" max="46" defaultValue="23" onChange={this.timeSliderChange} ></input>
-              
+            timeSelector = <div>Select times for {this.state.daySelected.str}<p />
+            
+            <div className="selectTimesWrapper-right-inner">
+            {allTimes}
+            </div>
+
+            <div className="selectTimesWrapper-right-inner">
+            {timesSelectedMap}
+            </div>
+
             </div>;
         }
 
@@ -235,7 +277,8 @@ class SetTime extends React.Component {
                 </div>
                 </div>
                 <div className="setTimeWrapper-right">
-                {timeSelector}
+                {timeSelector}<p />
+                {resetBtn}
                 </div>
                 </div>
                 </div>);
